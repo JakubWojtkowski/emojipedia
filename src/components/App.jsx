a61c1b6dbd5ae0ba669cd13236a3bbd1a5a3b49a
wojtko <j.w2001@onet.pl>
2023-03-30 12:50:28 +0200

    Refactoring Code. Improve some functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,14 @@ import emojis from "../emojipedia";
 import Entry from "../components/Entry";
 import Header from "../components/Header";
 
-function createEntry(emoji) {
-  return (
+const createEntry = (emoji => 
     <Entry
       key={emoji.id}
       emoji={emoji.emoji}
       name={emoji.name}
       meaning={emoji.meaning}
-    />
-  );
-}
+    /> );
+
 
 function App() {
   return (
